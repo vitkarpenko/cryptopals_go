96ab798e00fe7dbd4a41808c0567fa1be713a0c8
vitkarpenko <g24226838b>
2021-04-09 20:06:45 +0300

    A bit of tests.

--- set1/challenge6_test.go
+++ set1/challenge6_test.go
@@ -29,3 +29,55 @@ func Test_hammingDistanceTwoStrings(t *testing.T) {
 		})
 	}
 }
+
+func Test_hammingDistanceTwoBytes(t *testing.T) {
+	type args struct {
+		first  byte
+		second byte
+	}
+	tests := []struct {
+		name         string
+		args         args
+		wantDistance int
+	}{
+		{
+			name: "0_1",
+			args: args{
+				first:  0,
+				second: 1,
+			},
+			wantDistance: 1,
+		},
+		{
+			name: "0_2",
+			args: args{
+				first:  0,
+				second: 2,
+			},
+			wantDistance: 1,
+		},
+		{
+			name: "1_2",
+			args: args{
+				first:  1,
+				second: 2,
+			},
+			wantDistance: 2,
+		},
+		{
+			name: "8_19",
+			args: args{
+				first:  8,
+				second: 19,
+			},
+			wantDistance: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotDistance := hammingDistanceTwoBytes(tt.args.first, tt.args.second); gotDistance != tt.wantDistance {
+				t.Errorf("hammingDistanceTwoBytes() = %v, want %v", gotDistance, tt.wantDistance)
+			}
+		})
+	}
+}
